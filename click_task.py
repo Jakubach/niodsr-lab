0dfe6603510424581f6ea9ab012634abf01273c9
Jakubach <jakubach@gmail.com>
2022-11-23 22:59:03 +0100

    Create click_task.py

---
+++ click_task.py
@@ -0,0 +1,19 @@
+import click
+import datetime
+import random
+
+@click.command()
+@click.option('--path', default='/home/jakub/Desktop/my_file.txt', help='Path to the file')
+@click.option('--time',default=1,help='Where to save.')
+
+def hello(path, time):
+    previous_time = datetime.datetime.now()
+    while(1):
+        current_time = datetime.datetime.now()
+        if((current_time - previous_time).seconds >= time):
+            random_number = random.random()
+            with open(path,'a') as file:
+                file.write(str(current_time) + ' : ' + str(random_number)+ '\n')
+            previous_time = current_time
+
+    
